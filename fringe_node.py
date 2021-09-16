730651cb4b0bf11cbbaa94570cb18f78ea27b101
Naveenan <naveenan.r.y@gmail.com>
2021-09-16 18:48:23 -0400

    Created a class for the objects contained in the Fringe

--- fringe_node.py
+++ fringe_node.py
@@ -1,4 +1,11 @@
 class Fringe_Node:
 
-    def __init__(self, curr_block, parent_block,):
-        
+    # curr_block: coordinates for the block [x, y]
+    # parent_block: coordinates for the block's parent block [x, y]
+    # priority: g(n) + h(n)
+    def __init__(self, curr_block, parent_block, priority):
+        self.curr_block = curr_block
+        self.parent_block = parent_block
+        self.priority = priority
+
+    
